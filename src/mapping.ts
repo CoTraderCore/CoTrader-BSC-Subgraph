75cc2c24994aefaae449fb772c2395ab807d7fbd
vishal kale <40729195+vishalkale151071@users.noreply.github.com>
2021-08-27 11:32:48 +0530

    First Push.

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,62 +1,57 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Contract,
   OwnershipTransferred,
   SmartFundAdded
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import {
+  Deposit,
+  Withdraw,
+  Trade
+} from "../generated/templates/SmartFund/SmartFund"
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+import { SmartFund as SF } from "../generated/templates";
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+import { SmartFund, SmartFundDeposit, SmartFundTrade, SmartFundWithdraw} from "../generated/schema"
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-  // Entity fields can be set based on event parameters
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.defiPortalAddress(...)
-  // - contract.exchangePortalAddress(...)
-  // - contract.getAllSmartFundAddresses(...)
-  // - contract.maximumSuccessFee(...)
-  // - contract.owner(...)
-  // - contract.permittedAddresses(...)
-  // - contract.platformFeeAddress(...)
-  // - contract.poolPortalAddress(...)
-  // - contract.smartFundERC20Factory(...)
-  // - contract.smartFundETHFactory(...)
-  // - contract.smartFunds(...)
-  // - contract.totalSmartFunds(...)
+export function handleSmartFundAdded(event: SmartFundAdded): void {
+  SF.create(event.params.smartFundAddress);
+  
+  let smartfund = new SmartFund(event.params.smartFundAddress.toHex());
+  smartfund.owner = event.params.owner;
+  smartfund.save();  
 }
 
-export function handleSmartFundAdded(event: SmartFundAdded): void {}
+export function handleDeposit(event : Deposit): void {
+  let smartFundDeposit = new SmartFundDeposit(event.transaction.hash.toHex());
+  smartFundDeposit.smartFundAddress = event.transaction.to.toHex();
+  smartFundDeposit.user = event.params.user;
+  smartFundDeposit.amount = event.params.amount;
+  smartFundDeposit.sharesReceived = event.params.sharesReceived;
+  smartFundDeposit.totalShares = event.params.totalShares;
+  smartFundDeposit.timestamp = event.block.timestamp;
+  smartFundDeposit.save()
+}
+
+export function handleWithdraw(event : Withdraw): void {
+  let smartFundWithdraw = new SmartFundWithdraw(event.transaction.hash.toHex());
+  smartFundWithdraw.smartFundAddress = event.transaction.to.toHex();
+  smartFundWithdraw.user = event.params.user;
+  smartFundWithdraw.sharesRemoved = event.params.sharesRemoved;
+  smartFundWithdraw.totalShares = event.params.totalShares;
+  smartFundWithdraw.timestamp = event.block.timestamp;
+  smartFundWithdraw.save()
+}
+
+export function handleTrade(event : Trade): void {
+  let smartFundTrade = new SmartFundTrade(event.transaction.hash.toHex());
+  smartFundTrade.smartFundAddress = event.transaction.to.toHex();
+  smartFundTrade.source = event.params.src;
+  smartFundTrade.sourceAmount = event.params.srcAmount;
+  smartFundTrade.destination = event.params.dest;
+  smartFundTrade.destinationAmount = event.params.destReceived;
+  smartFundTrade.timestamp = event.block.timestamp;
+  smartFundTrade.save()
+}
